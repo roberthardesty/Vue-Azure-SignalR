f10edd4c91f839ef09bbd6dfea2ae9ed6e7edf52
Robert Hardesty <robert.hardesty@javs.com>
2018-06-27 17:17:29 -0400

    using an index for exporting en masse

---
+++ ipman/ClientApp/entity/index.ts
@@ -0,0 +1,12 @@
+export {default as Department} from './Department';
+export {default as Post} from './Post';
+export {default as PostChoice} from './PostChoice';
+export {default as Role} from './Role';
+export {default as SiteAccount} from './SiteAccount';
+export {default as Tag} from './Tag';
+export {default as UserAccount} from './UserAccount';
+export {default as Vote} from './Vote';
+export {default as Wager} from './Wager';
+
+export * from './lookups';
+export * from './join';
---
+++ ipman/ClientApp/entity/join/index.ts
@@ -0,0 +1,4 @@
+export {default as PostTag} from './PostTag';
+export {default as PostWager} from './PostWager';
+export {default as SiteAccountUserAccount} from './SiteAccountUserAccount';
+export {default as SiteAccountUserAccountDepartment} from './SiteAccountUserAccountDepartment';
