fa7d0c2eceac89a839a91dcd1b9d4d84063df550
Robert Hardesty <robert.hardesty@javs.com>
2018-07-01 13:29:03 -0400

    validate user context request response models

---
+++ ipman.shared/WebServiceModels/ValidateUserContextRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ipman.shared.WebServiceModels
+{
+    public class ValidateUserContextRequest
+    {
+        [Required]
+        public string Token { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
---
+++ ipman.shared/WebServiceModels/ValidateUserContextResponse.cs
@@ -0,0 +1,13 @@
+﻿using ipman.shared.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ipman.shared.WebServiceModels
+{
+    public class ValidateUserContextResponse: BaseResponse
+    {
+        public string NewToken { get; set; }
+        public UserAccount UserAcount { get; set; }
+    }
+}
